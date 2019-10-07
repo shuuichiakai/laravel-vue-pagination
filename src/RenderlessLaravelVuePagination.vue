e614f45574e43dbb0f46a81552c4fc93aaa08ec9
TAD <tad@TADs-Mac-mini.local>
2019-10-07 21:29:33 +0800

    rebuild

--- src/RenderlessLaravelVuePagination.vue
+++ src/RenderlessLaravelVuePagination.vue
@@ -122,15 +122,15 @@ export default {
             if (page === '...') {
                 return;
             }
-            if(this.data.post_type !== ''){
+            if(this.data.hasOwnProperty('post_type')){
               this.$emit('pagination-change-page', page + '&post_type=' + this.data.post_type);
             }
-            if(this.data.taxonomy !== ''){
+            if(this.data.hasOwnProperty('taxonomy')){
               this.$emit('pagination-change-page', page + '&taxonomy=' + this.data.taxonomy);
             }
-            if(this.data.taxonomy === '' && this.data.post_type === ''){
-              this.$emit('pagination-change-page', page )
-            }
+            // if(this.data.taxonomy === '' && this.data.post_type === ''){
+            //   this.$emit('pagination-change-page', page )
+            // }
         }
     },
 
